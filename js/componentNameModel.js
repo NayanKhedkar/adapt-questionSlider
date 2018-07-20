e808155165ce06be953fbd377f48e484d84bc775
Nayan Khedkar <nayan.khedkar@exultcorp.com>
2018-07-20 19:41:45 +0530

    init

--- js/componentNameModel.js
+++ js/componentNameModel.js
@@ -1,8 +1,8 @@
 define([
     'core/js/models/questionModel'
 ], function(QuestionModel) {
-    
-    var ComponentNameModel = QuestionModel.extend({
+
+    var QuestionSliderModel = QuestionModel.extend({
 
         /**
          * Used to restore the user's answers when revisiting the page or course
@@ -15,18 +15,38 @@ define([
             // The user answer is retrieved here
             // This value can then be used to set individual answers on items
             var userAnswer = this.get("_userAnswer");
-
+            this.updateSubItemState(userAnswer);
             this.setQuestionAsSubmitted();
             this.markQuestion();
             this.setScore();
             this.setupFeedback();
         },
 
+        updateSubItemState: function(userAnswer) {
+            var items = this.get('_items');
+            _.each(items, function(item, index) {
+                _.each(item._subItems, function(subItem, subIndex) {
+                    subItem._selectedAnswer = userAnswer.pop() === 1 ? 'true' : 'false';
+                }, this);
+            }, this);
+
+        },
+
         /**
          * Used to check if the user is allowed to submit the question
          * @returns {boolean}
          */
-        canSubmit: function() {},
+        canSubmit: function() {
+            var items = this.get('_items');
+            var selected = [];
+            _.each(items, function(item, index) {
+                selected.push(_.every(item._subItems, function(subItem) {
+                    return subItem._isSelected;
+                }));
+            }, this);
+            var completedSlide = _.without(selected, false);
+            return completedSlide.length !== 0 && completedSlide.length == items.length;
+        },
 
         /**
          * This evaluates the user's answer and stores the value
@@ -34,16 +54,27 @@ define([
          */
         storeUserAnswer: function() {
             // Expand on this to retrieve the user's answer as a single value
-            var userAnswer;
+            this.set("_userAnswer", this.getSelectedAnswer());
+        },
 
-            this.set("_userAnswer", userAnswer);
+        getSelectedAnswer: function() {
+            var items = this.get('_items');
+            var selectedAnswer = [];
+            _.each(items, function(item, index) {
+                _.each(item._subItems, function(subItem) {
+                    selectedAnswer.push(subItem._selectedAnswer === 'true' ? 1 : 0);
+                }, this);
+            }, this);
+            return selectedAnswer;
         },
 
         /**
          * Used to establish if the question is correct or not
          * @returns {boolean}
          */
-        isCorrect: function() {},
+        isCorrect: function() {
+
+        },
 
         /**
          * Used to set the score based upon the _questionWeight
@@ -67,6 +98,6 @@ define([
         resetUserAnswer: function() {},
     });
 
-    return ComponentNameModel;
+    return QuestionSliderModel;
 
 });
